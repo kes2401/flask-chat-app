041926a447ac82e066ef38fd0c9c9b6f7d511219
Keith Scully <keithscully83@gmail.com>
2019-02-28 21:50:42 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flask Chat App
+
+This is a Flask chat application written in Flask. The purpose of this
+application is to take data from a URL and store the information in a list
+
---
+++ run.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Hello There!</h1>"
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
+
+app.run(host = os.getenv('IP'), post = int(os.getenv('PORT')), debug = True)
