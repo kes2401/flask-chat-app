64d9c335be379ee930f617d7af30e44f3aac2666
Keith Scully <keithscully83@gmail.com>
2019-03-02 18:07:29 +0000

    add session cookies for user

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 from datetime import datetime
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "randomstring123"
 
 messages = []
 
@@ -15,9 +16,16 @@ def get_all_messages():
     """Get all of the messages and separate using a <br> tag"""
     return "<br>".join(messages)
     
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def index():
     """Main page with instructions"""
+    
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+    
+    if "username" in session:
+        return redirect(session["username"])
+        
     return render_template("index.html")
 
 @app.route('/<username>')
